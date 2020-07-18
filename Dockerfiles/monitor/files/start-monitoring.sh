31e605c047d4e4f4994001f9bbf12a508dc468ee
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2020-07-18 15:16:29 -0400

    Removed background job...

--- Dockerfiles/monitor/files/start-monitoring.sh
+++ Dockerfiles/monitor/files/start-monitoring.sh
@@ -3,4 +3,4 @@
 node_exporter &
 sleep 3
 
-prometheus --config.file=/root/config/prometheus.yml --web.listen-address=:3200 &
+prometheus --config.file=/root/config/prometheus.yml --web.listen-address=:3200
