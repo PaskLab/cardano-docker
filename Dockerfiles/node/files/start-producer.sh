637da527660583ffc7717825e9aa76e5afe0f7a5
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2021-07-22 07:52:20 -0400

    Fixed wrong bash trap usage.

--- Dockerfiles/node/files/start-producer.sh
+++ Dockerfiles/node/files/start-producer.sh
@@ -17,12 +17,14 @@ _term() {
   kill -SIGINT $PID
 }
 
+trap _term SIGTERM SIGINT
+
 echo "Starting Cardano Producer Node ..."
 cardano-node +RTS -N4 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -RTS run --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG} --shelley-kes-key ${KES} --shelley-vrf-key ${VRF} --shelley-operational-certificate ${CERT} &
 
 PID=$!
 wait $PID
-trap _term SIGTERM SIGINT
+trap - SIGTERM SIGINT
 wait $PID
 sleep 5
 EXIT_STATUS=$?
--- Dockerfiles/node/files/start-relay.sh
+++ Dockerfiles/node/files/start-relay.sh
@@ -14,12 +14,14 @@ _term() {
   kill -SIGINT $PID
 }
 
+trap _term SIGTERM SIGINT
+
 echo "Starting Cardano Relay Node ..."
 cardano-node run +RTS -N4 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -RTS --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG} &
 
 PID=$!
 wait $PID
-trap _term SIGTERM SIGINT
+trap - SIGTERM SIGINT
 wait $PID
 sleep 5
 EXIT_STATUS=$?
