430a290a2a75691da1532432d344ca6fdcc62350
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2021-07-27 08:48:10 -0400

    Fixed Topology startup script.

--- Dockerfiles/node/files/start-with-topology.sh
+++ Dockerfiles/node/files/start-with-topology.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
+HOSTADDR=0.0.0.0
+BASE_DIR=/cardano
+DB_PATH=${BASE_DIR}/db
+SOCKET_PATH=${BASE_DIR}/socket/node.sock
+CONFIG_DIR=${BASE_DIR}/config
+TOPOLOGY=${CONFIG_DIR}/topology.json
+CONFIG=${CONFIG_DIR}/config.json
+PORT=$(cat ${CONFIG_DIR}/port.txt)
+
+_term() {
+  echo "Stopping Cardano Relay Node ..."
+  kill -SIGINT $PID
+}
+
+trap _term SIGTERM SIGINT
+
+echo "Starting Topology Updater ..."
 topologyUpdater.sh &
-start-relay.sh
+echo "Starting Cardano Relay Node ..."
+cardano-node run +RTS -N4 -RTS --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG} &
+
+PID=$!
+wait $PID
+trap - SIGTERM SIGINT
+wait $PID
+sleep 5
+EXIT_STATUS=$?
+
+echo "Exit Status: ${EXIT_STATUS}"
