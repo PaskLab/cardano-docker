e9f4bd5e4003c1ffd969a71ffad5d97cad137851
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2021-12-20 17:25:16 -0500

    Changed RTS -N flag.

--- Dockerfiles/node/files/start-with-topology.sh
+++ Dockerfiles/node/files/start-with-topology.sh
@@ -19,7 +19,7 @@ trap _term SIGTERM SIGINT
 echo "Starting Topology Updater ..."
 topologyUpdater.sh &
 echo "Starting Cardano Relay Node ..."
-cardano-node run +RTS -N3 -RTS --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG} &
+cardano-node run +RTS -N4 -RTS --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG} &
 
 PID=$!
 wait $PID
